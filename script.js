2314be13406e6f21909af55494fb6cb44b1c7aa6
PaFtrSickl <113658877+PaFtrSickl@users.noreply.github.com>
2024-02-03 16:54:04 +0000

    Create script.js
    Added A JavaScript file.

---
+++ script.js
@@ -0,0 +1,7 @@
+// This is a simple JavaScript file
+// You can add your JavaScript code here
+
+// Example: Display an alert when the page loads
+window.onload = function() {
+    alert("Welcome to the Pushup Tracker!");
+};
