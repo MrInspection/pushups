ded0556052380b1249b76a373b8f6f07970a827c
Wesley Max <48810791+vim-wesley@users.noreply.github.com>
2024-02-11 14:39:10 -0300

    Fix - Adds scripts that solve the locale problem

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Fix Problems with Locale
+if (navigator.language !== "en-US") {
+  // Get the array from local storage
+  let array = JSON.parse(localStorage.getItem("History")); // replace 'yourArrayKey' with the actual key
+
+  // Iterate over the array
+  array.forEach((item) => {
+    // Parse the date string
+    let date = new Date(item.date);
+
+    // Format the date in 'M/D/YYYY' format
+    let formattedDate =
+      date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+
+    // Update the date in the object
+    item.date = formattedDate;
+  });
+
+  // Save the updated array back to local storage
+  localStorage.setItem("History", JSON.stringify(array));
+}
